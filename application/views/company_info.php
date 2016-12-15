ecd479f1fb8884103115b93c07aa8062e1b0e09a
Asus_laptop\Asus <suwareerat1994@gmail.com>
2016-12-15 18:34:31 +0700

    test update

--- application/views/company_info.php
+++ application/views/company_info.php
@@ -7,11 +7,11 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<title>ข้อมูลการคืนสินค้า</title>
+<title>ข้อมูลการคืนสินค้าxxxx</title>
 <!-- Meta tag Keywords -->
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<meta name="keywords" content="Sales Inquiry Form Responsive Widget Template, Bootstrap Web Templates, Flat Web Templates, Android Compatible web template, 
+<meta name="keywords" content="Sales Inquiry Form Responsive Widget Template, Bootstrap Web Templates, Flat Web Templates, Android Compatible web template,
 Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyEricsson, Motorola web design" />
 <script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false);
 function hideURLbar(){ window.scrollTo(0,1); } </script>
@@ -36,7 +36,7 @@ function hideURLbar(){ window.scrollTo(0,1); } </script>
             <ul>
 	         <li class="text">รหัสบริษัท  :  </li>
 				<li><input name="comp_id" type="text" required></li>
-		    </ul> 
+		    </ul>
             <ul>
 	         <li class="text">ชื่อบริษัท  :  </li>
 				<li><input name="comp_name" type="text" required></li>
@@ -53,27 +53,27 @@ function hideURLbar(){ window.scrollTo(0,1); } </script>
 			  <li class="text">Email  *  :  </li>
 				<li><input name="E-mail" type="text" required></li>
 			</ul>
-			
+
 			<div class="clear"></div>
 		  <div class="agile-submit">
                  <?php echo "<input type=\"Submit\" name=\"add\" value=\"เพิ่ม\">";?>
                  <?php echo "<input type=\"Submit\" name=\"delete\" value=\"ลบ\">";?>
                  <?php echo "<input type=\"Submit\" name=\"edit\" value=\"แก้ไข\">";?>
                  <?php echo "<input type=\"Submit\" name=\"save\" value=\"บันทึก\">";?>
-                 <?php echo "<input type=\"Submit\" name=\"back\" value=\"กลับไปหน้าแรก\">";?>				
-		  </div>        			
+                 <?php echo "<input type=\"Submit\" name=\"back\" value=\"กลับไปหน้าแรก\">";?>
+		  </div>
 			 <ul>
 			  <li>
 			    <table width="849" border="1">
 			      <tr>
                     <th width="45" height="24" scope="col">No.</th>
-			        <th width="67" scope="col">รหัสบริษัท</th> 
-                    <th width="168" scope="col">ชื่อบริษัท</th>			        
+			        <th width="67" scope="col">รหัสบริษัท</th>
+                    <th width="168" scope="col">ชื่อบริษัท</th>
 			        <th width="227" scope="col">ที่อยู่</th>
                     <th width="137" scope="col">Phone Number </th>
 			        <th width="165" scope="col">Email </th>
-			        
-			        
+
+
 		          </tr>
 		          <tr>
 			        <td>&nbsp;</td>
@@ -81,13 +81,13 @@ function hideURLbar(){ window.scrollTo(0,1); } </script>
 			        <td>&nbsp;</td>
 			        <td>&nbsp;</td>
 			        <td>&nbsp;</td>
-			        <td>&nbsp;</td>                   
+			        <td>&nbsp;</td>
 		          </tr>
-		       </table>           
+		       </table>
 	        </li>
 		</ul>
           </form>
-		</div>	
+		</div>
 	</div>
 <!--//main-->
 <!--footer-->
